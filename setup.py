3863b9834ffb003ade85c2517b1e81e8398d1602
Phylake1337 <ahmed1337saied@gmail.com>
2021-09-23 01:02:35 +0200

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='practicing end-to-end ds project',
+    author='ahmed saied',
+    license='',
+)
